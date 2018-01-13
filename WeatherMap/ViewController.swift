d730b98860b3e658911c4109fff9896ea3d19b3e
Johnson <johnson6510@gmail.com>
2018-01-14 05:32:46 +0800

    add PM2.5 map view for Taiwan

--- WeatherMap/ViewController.swift
+++ WeatherMap/ViewController.swift
@@ -18,6 +18,8 @@ protocol HandleMapSearch {
 }
 
 class ViewController: UIViewController, MKMapViewDelegate {
+    let urlTwUv = "http://opendata2.epa.gov.tw/UV/UV.json"
+    var isPm2d5: Bool = false
 
     let OpenWeatherAPIKey = "cc97bcab2f8e89e1d4a5af6e6029022f"
 
@@ -30,6 +32,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
     let entity = NSFetchRequest<NSFetchRequestResult>(entityName: "StoredPlace")
     var myLocation = MKPointAnnotation()
     var selectedAnnotation = MKPointAnnotation()
+    var pm2d5Annotations = [CustomPointAnnotation()]
     
     @IBAction func returnHome(_ sender: Any) {
         if CLLocationManager.locationServicesEnabled() {
@@ -38,7 +41,164 @@ class ViewController: UIViewController, MKMapViewDelegate {
             mapView.setRegion(region, animated: true)
         }
     }
+    
+    let sites: [String: [String: Double]] = [
+        "麥寮": ["longitude": 120.25626620000003 ,"latitude": 23.7485306],
+        "關山": ["longitude": 121.16463390000001 ,"latitude": 23.0495603],
+        "馬公": ["longitude": 119.57746159999999 ,"latitude": 23.5706269],
+        "金門": ["longitude": 118.3285644        ,"latitude": 24.3487792],
+        "馬祖": ["longitude": 119.95166519999998 ,"latitude": 26.160243 ],
+        "埔里": ["longitude": 120.96468660000005 ,"latitude": 23.9932872],
+        "復興": ["longitude": 120.31172220000008 ,"latitude": 22.6083352],
+        "永和": ["longitude": 121.51453530000003 ,"latitude": 25.0103251],
+        "竹山": ["longitude": 120.68900550000001 ,"latitude": 23.712201 ],
+        "中壢": ["longitude": 121.20539630000007 ,"latitude": 24.9721514],
+        "三重": ["longitude": 121.48671139999999 ,"latitude": 25.0614534],
+        "冬山": ["longitude": 121.75374039999997 ,"latitude": 24.631919 ],
+        "宜蘭": ["longitude": 121.73775019999994 ,"latitude": 24.7021073],
+        "陽明": ["longitude": 121.51727219999998 ,"latitude": 25.0921827],
+        "花蓮": ["longitude": 121.61119489999999 ,"latitude": 23.9910732],
+        "臺東": ["longitude": 121.14381520000006 ,"latitude": 22.7613207],
+        "恆春": ["longitude": 120.74476379999999 ,"latitude": 22.0008277],
+        "潮州": ["longitude": 120.54026720000002 ,"latitude": 22.5514922],
+        "屏東": ["longitude": 120.5487597        ,"latitude": 22.5519759],
+        "小港": ["longitude": 120.36084549999998 ,"latitude": 22.5553185],
+        "前鎮": ["longitude": 120.31472080000003 ,"latitude": 22.5970794],
+        "前金": ["longitude": 120.29453620000004 ,"latitude": 22.6254162],
+        "左營": ["longitude": 120.29165239999998 ,"latitude": 22.6877358],
+        "楠梓": ["longitude": 120.30318710000006 ,"latitude": 22.7175372],
+        "林園": ["longitude": 120.4011911        ,"latitude": 22.4986756],
+        "大寮": ["longitude": 120.4011911        ,"latitude": 22.584481 ],
+        "鳳山": ["longitude": 120.3493158        ,"latitude": 22.6113591],
+        "仁武": ["longitude": 120.36084549999998 ,"latitude": 22.6947932],
+        "橋頭": ["longitude": 120.30895409999994 ,"latitude": 22.7539012],
+        "美濃": ["longitude": 120.55093579999993 ,"latitude": 22.885385 ],
+        "臺南": ["longitude": 120.22702770000001 ,"latitude": 22.9997281],
+        "安南": ["longitude": 120.13583459999995 ,"latitude": 23.0585336],
+        "善化": ["longitude": 120.30895409999994 ,"latitude": 23.1402613],
+        "新營": ["longitude": 120.30895409999994 ,"latitude": 23.3119567],
+        "嘉義": ["longitude": 120.44911130000003 ,"latitude": 23.4800751],
+        "臺西": ["longitude": 120.19356629999993 ,"latitude": 23.7229714],
+        "朴子": ["longitude": 120.25704210000004 ,"latitude": 23.4464152],
+        "新港": ["longitude": 120.3550808        ,"latitude": 23.538123 ],
+        "崙背": ["longitude": 120.3531749        ,"latitude": 23.7601594],
+        "斗六": ["longitude": 120.54090889999998 ,"latitude": 23.7077947],
+        "南投": ["longitude": 120.67750539999997 ,"latitude": 23.9179637],
+        "二林": ["longitude": 120.4011911        ,"latitude": 23.9141358],
+        "線西": ["longitude": 120.46220160000007 ,"latitude": 24.1316695],
+        "彰化": ["longitude": 120.5624474        ,"latitude": 24.0716583],
+        "西屯": ["longitude": 120.6424333        ,"latitude": 24.1769764],
+        "忠明": ["longitude": 120.66361430000006 ,"latitude": 24.1588789],
+        "大里": ["longitude": 120.68121139999994 ,"latitude": 24.1046899],
+        "沙鹿": ["longitude": 120.58546739999997 ,"latitude": 24.2377939],
+        "豐原": ["longitude": 120.72235720000003 ,"latitude": 24.2521156],
+        "三義": ["longitude": 120.76947599999994 ,"latitude": 24.3892633],
+        "苗栗": ["longitude": 120.81543579999993 ,"latitude": 24.5711502],
+        "頭份": ["longitude": 120.90248359999998 ,"latitude": 24.6884438],
+        "新竹": ["longitude": 120.96747979999998 ,"latitude": 24.8138287],
+        "竹東": ["longitude": 121.04497679999997 ,"latitude": 24.774922 ],
+        "湖口": ["longitude": 121.04497679999997 ,"latitude": 24.8814458],
+        "龍潭": ["longitude": 121.20539630000007 ,"latitude": 24.8444927],
+        "平鎮": ["longitude": 121.20539630000007 ,"latitude": 24.9296022],
+        "觀音": ["longitude": 121.11375440000006 ,"latitude": 25.0359365],
+        "大園": ["longitude": 121.19394499999999 ,"latitude": 25.0492632],
+        "桃園": ["longitude": 121.30097980000005 ,"latitude": 24.9936281],
+        "大同": ["longitude": 121.51130639999997 ,"latitude": 25.0627243],
+        "松山": ["longitude": 121.56386210000005 ,"latitude": 25.0541591],
+        "古亭": ["longitude": 121.52747870000007 ,"latitude": 25.021694 ],
+        "萬華": ["longitude": 121.49702939999997 ,"latitude": 25.0262857],
+        "中山": ["longitude": 121.54270930000007 ,"latitude": 25.0792018],
+        "士林": ["longitude": 121.52460769999993 ,"latitude": 25.0950492],
+        "淡水": ["longitude": 121.44337059999998 ,"latitude": 25.1719805],
+        "林口": ["longitude": 121.38813779999998 ,"latitude": 25.0790108],
+        "菜寮": ["longitude": 121.49215600000002 ,"latitude": 25.060274 ],
+        "新莊": ["longitude": 121.41783469999996 ,"latitude": 25.0265985],
+        "板橋": ["longitude": 121.46184149999999 ,"latitude": 25.0114095],
+        "土城": ["longitude": 121.43803400000002 ,"latitude": 24.968371 ],
+        "新店": ["longitude": 121.53948220000007 ,"latitude": 24.978282 ],
+        "萬里": ["longitude": 121.63971839999999 ,"latitude": 25.1676024],
+        "汐止": ["longitude": 121.63971839999999 ,"latitude": 25.0616059],
+        "基隆": ["longitude": 121.73918329999992 ,"latitude": 25.1276033],
+   ]
 
+    @IBAction func showPM2d5(_ sender: Any) {
+        isPm2d5 = !isPm2d5
+        if isPm2d5 {
+            //create annotations
+            let parameters: Parameters = ["foo": "bar"]
+            let urlPm2d5 = "http://opendata.epa.gov.tw/ws/Data/ATM00625/?$format=json"
+            Alamofire.request(urlPm2d5, parameters: parameters).responseJSON { response in
+                DispatchQueue.main.async {
+                    guard response.result.isSuccess else {
+                        let errorMessage = response.result.error?.localizedDescription
+                        print(errorMessage!)
+                        return
+                    }
+                    guard let JSON = response.result.value as? Array<Dictionary<String,String>> else {
+                        print("JSON formate error")
+                        return
+                    }
+                    //print(JSON)
+                    for key in 0..<JSON.count {
+                        if let site = JSON[key]["Site"] {
+                            let country = JSON[key]["county"]
+                            let pm25 = JSON[key]["PM25"]
+                            //print(self.sites[site]!["longitude"] ?? 0)
+                            //print(self.sites[site]!["latitude"] ?? 0)
+                            //print(country! + ", " + site)
+                            //print(pm25 ?? -1)
+                            var coordinates = CLLocationCoordinate2D()
+                            coordinates.longitude = self.sites[site]!["longitude"]!
+                            coordinates.latitude = self.sites[site]!["latitude"]!
+                            self.addPM2d5Annotation(coordinates: coordinates, title: country! + ", " + site, subtitle: pm25!)
+
+                        }
+                    }
+                }
+            }
+        } else {
+            //remove annotations
+            for annotation in pm2d5Annotations {
+                self.mapView.removeAnnotation(annotation)
+            }
+        }
+    }
+    
+    func addPM2d5Annotation(coordinates: CLLocationCoordinate2D, title: String, subtitle: String) {
+        let annotation = CustomPointAnnotation()
+        annotation.coordinate = coordinates
+        annotation.title = title
+        annotation.subtitle = subtitle
+        annotation.imageName = "air5"
+        if let pm25 = Int(subtitle) {
+            switch pm25 {
+            case 0...11:
+                annotation.imageName = "air1"
+            case 12...23:
+                annotation.imageName = "air1"
+            case 24...35:
+                annotation.imageName = "air2"
+            case 36...41:
+                annotation.imageName = "air2"
+            case 42...47:
+                annotation.imageName = "air3"
+            case 48...53:
+                annotation.imageName = "air3"
+            case 54...58:
+                annotation.imageName = "air4"
+            case 59...64:
+                annotation.imageName = "air4"
+            case 65...70:
+                annotation.imageName = "air5"
+            case 71...999:
+                annotation.imageName = "air5"
+            default:
+                annotation.imageName = "air5"
+            }
+        }
+        self.mapView.addAnnotation(annotation)
+        pm2d5Annotations.append(annotation)
+    }
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -110,7 +270,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         annotation.title = "Loading…"
         annotation.subtitle = "Loading…"
         //https://openweathermap.org/weather-conditions
-        annotation.imageName = "50d.png"
+        annotation.imageName = "50d"
         
         let url = "http://api.openweathermap.org/data/2.5/weather?lat=\(coordinates.latitude)&lon=\(coordinates.longitude)&APIKEY=\(OpenWeatherAPIKey)&units=metric"
         
@@ -125,12 +285,12 @@ class ViewController: UIViewController, MKMapViewDelegate {
                     print("JSON formate error")
                     return
                 }
-                //print(JSON)
+                print(JSON)
                 annotation.title = "\(JSON["name"]!), \(JSON["sys"]!["country"]! ?? "")"
                 annotation.subtitle = "\(JSON["main"]!["temp"]! ?? 0)(\(JSON["main"]!["temp_min"]! ?? 0)~\(JSON["main"]!["temp_max"]! ?? 0)) ℃"
 
                 if let weather = JSON["weather"] as? [[String: AnyObject]] {
-                    annotation.imageName = weather[0]["icon"] as! String + ".png"
+                    annotation.imageName = weather[0]["icon"] as! String// + ".png"
                     annotation.subtitle! += ", \(weather[0]["description"] ?? "Unknow" as AnyObject)"
                     //print (annotation.imageName)
                     self.mapView.addAnnotation(annotation)
@@ -149,7 +309,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         annotation.title = "Loading…"
         annotation.subtitle = "Loading…"
         //https://openweathermap.org/weather-conditions
-        annotation.imageName = "50d.png"
+        annotation.imageName = "50d"
         
         let url = "http://api.openweathermap.org/data/2.5/weather?lat=\(coordinates.latitude)&lon=\(coordinates.longitude)&APIKEY=\(OpenWeatherAPIKey)&units=metric"
         
@@ -169,7 +329,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
                 annotation.subtitle = "\(JSON["main"]!["temp"]! ?? 0)(\(JSON["main"]!["temp_min"]! ?? 0)~\(JSON["main"]!["temp_max"]! ?? 0)) ℃"
                 
                 if let weather = JSON["weather"] as? [[String: AnyObject]] {
-                    annotation.imageName = weather[0]["icon"] as! String + ".png"
+                    annotation.imageName = weather[0]["icon"] as! String// + ".png"
                     annotation.subtitle! += ", \(weather[0]["description"] ?? "Unknow" as AnyObject)"
                     //print (annotation.imageName)
                     self.mapView.addAnnotation(annotation)
@@ -334,7 +494,6 @@ extension ViewController {
                 pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
                 pinView!.canShowCallout = true
                 
-                //bug (getDirection can not get correct location
                 pinView!.pinTintColor = UIColor.orange
                 let button = UIButton(frame: CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 30, height: 30)))
                 button.setBackgroundImage(UIImage(named: "car"), for: .normal)
